50aa3d2e631d1df054f0d9a3136eb1f5a10a41a0
carlgira <carlgira@gmail.com>
2023-03-09 17:40:50 +0100

    clean

--- config-req.sh
+++ config-req.sh
@@ -1,6 +1,6 @@
 USER='ubuntu'
 sudo apt update
-sudo apt install wget unzip default-jdk apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install docker-compose wget unzip default-jdk apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
